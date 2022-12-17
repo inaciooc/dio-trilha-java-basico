692b5ad6e8af3b12c5afd6222f1d4f073c784744
inacio <inaciooc@gmail.com>
2022-12-17 16:45:59 -0300

    Treinamento

---
+++ dio-tipo-variaveis-java/src/Operadores.java
@@ -0,0 +1,7 @@
+public class Operadores{
+    public static void main(String[] args) {
+        String nomeCompleto = "Inacio" + " de Oliveira";
+
+        System.out.println(nomeCompleto);
+    }
+}
